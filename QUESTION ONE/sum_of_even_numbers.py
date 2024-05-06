498cd5d572d3128d6319a07dee128479e7299ba3
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-06 11:17:54 +0300

    Function for adding even numbers in a list

--- QUESTION ONE/sum_of_even_numbers.py
+++ QUESTION ONE/sum_of_even_numbers.py
@@ -1,6 +1,9 @@
 def sum_of_even_numbers(list):
+    """
+    This Python function calculates the sum of even numbers in a given list.
+    """
     sum = 0
     for number in list:
-        if number % 2 == 0:  # Check if the number is even
+        if number % 2 == 0:
             sum += number
     return sum
